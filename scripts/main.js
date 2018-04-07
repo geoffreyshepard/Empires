9aa505b45550a2f7315e65a67a117954a8d061a0
geoffreyshepard <geoffreyshepard@gmail.com>
2018-04-07 00:42:52 -0500

    Basic Game Loop - days, month, years

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,58 @@
 // Cache Dom Bindings
-
+var $timeDay     = $("span[data-time='day']"),
+    $timeMonth   = $("span[data-time='month']"),
+    $timeYear    = $("span[data-time='year']");
 
 // Game Settings and Initialization
 var G = {
 
-    setting: {},
-    init: {}
+    settings: {
+        currentTickSpeed: 50,
+        time: {
+            ticks: 0,
+            day: 1,
+            month: 1,
+            year: 1
+        }
+    },
+
+    init : function(){
+        this.loop()
+    },
+
+    loop : function(){
+        setTimeout(
+            function(){ 
+                G.updateTime(); 
+                G.loop();
+            },
+            this.settings.currentTickSpeed
+        );
+    }
 
 };
 
 
+G.updateTime = function(){
+    G.settings.time.ticks ++;
+
+	G.settings.time.day ++;
+	if(G.settings.time.day > 30){
+		G.settings.time.day = 1;
+		G.settings.time.month ++;
+		// TODO : Add Autosave Here
+		if(G.settings.time.month > 12){
+			G.settings.time.month = 1;
+			G.settings.time.year ++;
+		}
+    }
+    
+    $timeDay.html(G.settings.time.day);
+    $timeMonth.html(G.settings.time.month);
+    $timeYear.html(G.settings.time.year);
+}
 
 
 
 
+G.init();
